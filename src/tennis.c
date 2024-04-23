423d8a67f8ab1edac6444ef890acac4760a1aac8
Rafal C <rafalts86@gmail.com>
2024-04-23 16:52:30 +0200

    tennis: TDD green - player1 gets three points

--- src/tennis.c
+++ src/tennis.c
@@ -6,9 +6,15 @@ void tennis_point(enum player player)
 {
     if(player == PLAYER1)
     {
-        player1_points += 15;
+        if(player1_points < 30)
+        {
+            player1_points += 15;
+        }
+        else if(player1_points == 30)
+        {
+            player1_points += 10;
+        }
     }
-    
 }
 
 uint8_t tennis_get_points(enum player player)
