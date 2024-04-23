c4d01c791cf258154b19d5a64488cfe1c4830dc6
Rafal C <rafalts86@gmail.com>
2024-04-23 16:46:08 +0200

    tennis: TDD red - add test player1 gets two points

--- test/tennis/tennis_test.c
+++ test/tennis/tennis_test.c
@@ -20,3 +20,10 @@ TEST(tennis, Player1_point)
     tennis_point(PLAYER1);
     TEST_ASSERT_EQUAL(15, tennis_get_points(PLAYER1));
 }
+
+TEST(tennis, Player1_get_two_points)
+{
+    tennis_point(PLAYER1);
+    tennis_point(PLAYER1);
+    TEST_ASSERT_EQUAL(30, tennis_get_points(PLAYER1));
+}
